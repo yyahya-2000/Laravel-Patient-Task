37beef9e59a590f09bdffb96759053f2eafbc9de
yanal <yanal@keep-calm.ru>
2023-05-29 17:29:15 +0300

    initialize patient routes and controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PatientController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::post('/patient', [PatientController::class, 'create']);
+
+Route::get('/patients', [PatientController::class, 'index']);

e11cb63c32b8e00e2427ac705b3136d44d2c107b
yanal <yanal@keep-calm.ru>
2023-05-29 18:37:09 +0300

    add the logic to the index and create methods of Patient controller and model

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -2,17 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Patient;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class PatientController extends Controller
 {
-    public function index()
+    public function index(): JsonResponse
     {
-        return [];
+        $patients = Cache::get('patients');
+
+        if (!$patients) {
+            $patients = Patient::all();
+
+            Cache::put('patients', $patients, 5);
+        }
+
+        $response = $patients->map(function ($patient) {
+            return [
+                'name' => $patient->first_name . ' ' . $patient->last_name,
+                'birthdate' => $patient->birthdate->format('d.m.Y'),
+                'age' => $patient->age . ' ' . $patient->age_type,
+            ];
+        });
+
+        return response()->json($response);
     }
 
-    public function create(Request $request)
+    public function create(Request $request): JsonResponse
     {
-        return [];
+        $validatedData = $request->validate([
+            'first_name' => 'required|string',
+            'last_name' => 'required|string',
+            'birthdate' => 'required|date',
+        ]);
+
+        $patient = Patient::createPatient($validatedData['first_name'], $validatedData['last_name'], $validatedData['birthdate']);
+
+        $patients = Cache::get('patients', []);
+        $patients[] = $patient;
+        Cache::put('patients', $patients, 5);
+
+        return response()->json(['message' => 'Patient created successfully']);
     }
 }
--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -15,4 +16,42 @@ class Patient extends Model
         'age',
         'age_type',
     ];
+
+    public static function createPatient(string $firstName, string $lastName, $birthdate)
+    {
+        $birthdate = Carbon::parse($birthdate);
+
+        $age = self::calculateAge($birthdate);
+        $ageType = self::defineAgeType($birthdate);
+
+        $patient = new static();
+        $patient->first_name = $firstName;
+        $patient->last_name = $lastName;
+        $patient->birthdate = $birthdate;
+        $patient->age = $age;
+        $patient->age_type = $ageType;
+        $patient->save();
+
+        return $patient;
+    }
+
+    /**
+     * @param Carbon $birthdate
+     * @return float|int
+     */
+    public static function calculateAge(Carbon $birthdate): int|float
+    {
+        $now = Carbon::now();
+        return $birthdate->diffInDays($now) < 30 ? $birthdate->diffInDays($now) : ($birthdate->diffInMonths($now) < 12 ? $birthdate->diffInMonths($now) : $birthdate->diffInYears($now));
+    }
+
+    /**
+     * @param Carbon $birthdate
+     * @return string
+     */
+    public static function defineAgeType(Carbon $birthdate): string
+    {
+        $now = Carbon::now();
+        return $birthdate->diffInDays($now) < 30 ? 'день' : ($birthdate->diffInMonths($now) < 12 ? 'месяц' : 'год');
+    }
 }
